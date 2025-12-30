10b95ab320c241794085a87bfb22771a2e1cfa29
MangoCubes <10383115+MangoCubes@users.noreply.github.com>
2025-12-30 03:20:39 +0000

    Add new function, resolve key conflict

--- my/functions.el
+++ my/functions.el
@@ -1,7 +1,7 @@
 (defun my/terminal ()
   "Open a new terminal."
   (interactive)
-  (start-process "kitty" nil "kitty"))
+  (start-process "t" nil "t"))
 
 ;; Simplify yes-or-no prompt with simple y/n input
 (defun my/ynp (prompt)
@@ -12,6 +12,7 @@
 	  (if (eq answer ?n)
 		  nil
 		(ynp prompt)))))
+
 (defalias 'yes-or-no-p 'my/ynp)
 
 ;; Open terminal in a new tab
@@ -20,3 +21,20 @@
   (tab-new)  ;; Create a new tab
   (ansi-term "bash"))
 
+(defun my/clean-undo-tree ()
+"Clear current buffer's undo-tree.
+Undo-tree can cause problems with file encoding when characters
+are inserted that cannot be represented using the files current
+encoding. This is even the case when characters are only
+temporarily inserted, e.g. pasted from another source and then
+instantly deleted. In these situations it can be necessary to
+clear the buffers undo-tree before saving the file."
+  (interactive)
+  (let ((buff (current-buffer)))
+    (if (local-variable-p 'buffer-undo-tree)
+        (if (my/ynp "Clear buffer-undo-tree?")
+            (progn
+              (setq buffer-undo-tree nil)
+              (message "Cleared undo-tree of buffer: %s" (buffer-name buff)))
+          (message "Cancelled clearing undo-tree of buffer: %s" (buffer-name buff)))
+      (error "Buffer %s has no local binding of `buffer-undo-tree'" (buffer-name buff)))))
--- plugins/projectile.el
+++ plugins/projectile.el
@@ -4,4 +4,4 @@
 
 (projectile-mode +1)
 ;; Recommended keymap prefix on Windows/Linux
-(define-key projectile-mode-map (kbd "<leader>p") 'projectile-command-map)
+(define-key projectile-mode-map (kbd "<leader>r") 'projectile-command-map)
