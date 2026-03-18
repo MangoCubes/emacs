0bc85546b199f1b26d63acd6bf22f80710079926
MangoCubes <10383115+MangoCubes@users.noreply.github.com>
2026-03-18 23:36:29 +0900

    Set org roam directory properly

--- plugins/org/org-roam.el
+++ plugins/org/org-roam.el
@@ -1,6 +1,13 @@
 (straight-use-package 'org-roam)
 (require 'org-roam)
-(setq org-roam-directory (file-truename "~/Sync/Notes/Org"))
+
+(setq org-roam-directory
+      (file-truename
+       (expand-file-name
+        "Sync/Notes/Org"
+        (my/linux-or-win
+         (lambda () (getenv "HOME"))
+         (lambda () (expand-file-name "Documents" (getenv "USERPROFILE")))))))
 
 ;; Cache node IDs automatically
 (org-roam-db-autosync-mode)
