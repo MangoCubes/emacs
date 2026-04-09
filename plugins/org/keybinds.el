f266a2c25a58b9dc51811492dd4f5b15b8309f13
MangoCubes <10383115+MangoCubes@users.noreply.github.com>
2026-04-09 13:43:47 +0900

    Add the keybinds to my functions

--- plugins/org/keybinds.el
+++ plugins/org/keybinds.el
@@ -1,6 +1,9 @@
 (with-eval-after-load 'org
   (define-key org-mode-map (kbd "C-c l") 'org-store-link)
-  (define-key org-mode-map (kbd "<leader>o") 'my/org-mode-setup)
+  (define-key evil-normal-state-map (kbd "]m") 'org-forward-heading-same-level)
+  (define-key evil-normal-state-map (kbd "[m") 'org-backward-heading-same-level)
+  (define-key org-mode-map (kbd "<leader>os") 'my/org-mode-setup)
   (define-key org-mode-map (kbd "<leader>C") 'my/clean-undo-tree)
+  (define-key org-mode-map (kbd "<leader>on") 'my/org-create-file)
 )
 
