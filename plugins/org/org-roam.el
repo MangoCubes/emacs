56ac8b3477c61ffbf307fd9dc0b6180e4c495020
MangoCubes <10383115+MangoCubes@users.noreply.github.com>
2026-04-17 16:27:59 +0900

    Agenda and directory related stuff

--- plugins/org/org-roam.el
+++ plugins/org/org-roam.el
@@ -1,13 +1,20 @@
 (straight-use-package 'org-roam)
 (require 'org-roam)
 
-(setq org-roam-directory
-      (file-truename
-       (expand-file-name
-        "Sync/Notes/Org"
-        (my/linux-or-win
-         (lambda () (getenv "HOME"))
-         (lambda () (expand-file-name "Documents" (getenv "USERPROFILE")))))))
+(setq org-roam-directory my/org-root)
+
+(setq org-agenda-files
+      (directory-files-recursively
+       (expand-file-name "Daily" my/org-root)
+       "\\.org$"))
+
+(setq org-agenda-todo-list-sublevels t)
+(setq org-agenda-tags-todo-honor-ignore-options t)
+(setq org-agenda-show-all-dates t)
+(setq org-agenda-entry-text-maxlines 1)
+
+(setq org-agenda-sorting-strategy
+      '((todo deadline-up priority-down)))
 
 ;; Cache node IDs automatically
 (org-roam-db-autosync-mode)
