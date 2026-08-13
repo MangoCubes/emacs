82b2d60a5c43c6fc70a12ac465ca578af18c30ed
MangoCubes <10383115+MangoCubes@users.noreply.github.com>
2026-08-13 16:07:26 +0900

    Add new bind

--- plugins/org/keybinds.el
+++ plugins/org/keybinds.el
@@ -1,3 +1,8 @@
+(defun my/org-insert-child-heading ()
+  (interactive)
+  (org-insert-heading-respect-content)
+  (org-do-demote))
+
 (with-eval-after-load 'org
   (define-key org-mode-map (kbd "C-c l") 'org-store-link)
   (define-key org-mode-map (kbd "C-c a") 'org-agenda)
@@ -5,5 +10,5 @@
   (define-key evil-normal-state-map (kbd "[m") 'org-backward-heading-same-level)
   (define-key org-mode-map (kbd "<leader>os") 'my/org-mode-setup)
   (define-key org-mode-map (kbd "C-c n") 'my/org-create-file)
-)
+  (define-key org-mode-map (kbd "C-M-<return>") 'my/org-insert-child-heading))
 
