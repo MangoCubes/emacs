96f5aef10cdf45ddb4267054fe3a628a38305515
MangoCubes <10383115+MangoCubes@users.noreply.github.com>
2026-09-06 12:13:18 +0900

    Add more mail-related properties

--- plugins/notmuch.el
+++ plugins/notmuch.el
@@ -3,3 +3,15 @@
 
 (setq notmuch-directory "~/.mail")  ; Set your mail directory
 (setq notmuch-search-oldest-first nil) ; Show newest messages first
+
+(setq user-mail-address "admin@skew.ch")
+(setq user-full-name "Admin")
+
+(setq message-kill-buffer-on-exit t)
+(setq message-auto-save-directory "~/.mail/drafts")
+
+(setq send-mail-function 'sendmail-send-it)
+(setq sendmail-program "msmtp")
+(setq mail-specify-envelope-from t)
+(setq message-sendmail-envelope-from 'header)
+(setq mail-envelope-from 'header)
