d1dc8505a582dba059ea2992c7f8dd54b38c3c52
MangoCubes <10383115+MangoCubes@users.noreply.github.com>
2026-09-07 00:14:16 +0900

    Use emacs for sending mails

--- plugins/notmuch.el
+++ plugins/notmuch.el
@@ -3,3 +3,13 @@
 
 (setq notmuch-directory "~/.mail")  ; Set your mail directory
 (setq notmuch-search-oldest-first nil) ; Show newest messages first
+
+(defun my/mailto-handler (mailto-uri &rest _)
+  (interactive "sMailto URI: ")
+  (raise-frame)
+  (select-frame-set-input-focus (selected-frame))
+  (message-mailto mailto-uri))
+
+(setq mail-user-agent 'message-user-agent)
+
+(setq browse-url-mailto-function #'my/mailto-handler)
