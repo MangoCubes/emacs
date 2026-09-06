266ad730f3ed5c80ce8b5da120df6ff7f6490c49
MangoCubes <10383115+MangoCubes@users.noreply.github.com>
2026-09-06 12:19:45 +0900

    Auto delete autosave file

--- plugins/notmuch.el
+++ plugins/notmuch.el
@@ -15,3 +15,10 @@
 (setq mail-specify-envelope-from t)
 (setq message-sendmail-envelope-from 'header)
 (setq mail-envelope-from 'header)
+
+;; If the autosave file exists when the buffer is killed, delete it
+(defun my/remove-draft-mail ()
+  (when (and buffer-auto-save-file-name (file-exists-p buffer-auto-save-file-name))
+    (delete-file buffer-auto-save-file-name)))
+
+(add-hook 'message-mode-hook (lambda () (add-hook 'kill-buffer-hook #'my/remove-draft-mail nil t)))
